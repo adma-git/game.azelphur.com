7ff5f2e526655d5fba031f2cb076f3f6c72a0d7e
adma <28440591+adma-git@users.noreply.github.com>
2018-02-02 11:24:45 +1000

    Fix timezones

--- donations/processors.py
+++ donations/processors.py
@@ -3,13 +3,15 @@ from django.conf import settings
 from django.db.models import Sum, F, IntegerField
 from decimal import Decimal
 import datetime
+import calendar
 from django.utils import timezone
 from donations.models import PremiumDonationHistory
 
 def donations(request):
     now = timezone.now()
     amount = PremiumDonationHistory.objects.filter(
-        start_time__gt = datetime.date(now.year, now.month, 1)
+        start_time__gte = timezone.make_aware(datetime.datetime(now.year, now.month, 1)),
+        start_time__lte = timezone.make_aware(datetime.datetime(now.year, now.month, calendar.monthrange(now.year, now.month)[1], 23, 59, 59))
     ).aggregate(amount = Sum((F('keys') * 2) + F('dollars'), output_field=IntegerField()))['amount']
     if amount == None:
         amount = Decimal("0.0")
